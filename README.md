d8e211e2714d51afd9c34cdb2774c9ee9ee20bbe
SK <6290943+hckrtst@users.noreply.github.com>
2024-04-28 17:51:58 -0700

    xtensor support and update docs

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ This has notes and examples from https://karpathy.ai/zero-to-hero.html using C++
 * CMake 3.25+
 * g++ 12.2+
 
-## Third party dependencies
+## Third party dependencies (included as submodules)
 * spdlog
 * matplot
 
-## System dependencies
-* gnuplot
+## System dependencies (installed via package manager)
+* gnuplot (needed for matplot)
+* xtensor-dev (substitute for Numpy)
+
 
--- micrograd/src/main.cc
+++ micrograd/src/main.cc
@@ -1,17 +1,35 @@
 // std
 #include <cmath>
+#include <sstream>
 
 // deps
 #include "spdlog/spdlog.h"
 #include <matplot/matplot.h>
-
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xview.hpp>
 namespace mp = matplot;
 
 int main()
 {
     spdlog::info("Welcome to micrograd!");
     
+    // test xtensor
+    xt::xarray<double> arr1
+      {{1.0, 2.0, 3.0},
+       {2.0, 5.0, 7.0},
+       {2.0, 5.0, 7.0}};
+
+    xt::xarray<double> arr2
+      {5.0, 6.0, 7.0};
+
+    xt::xarray<double> res = xt::view(arr1, 1) + arr2;
+
+    std::ostringstream ss;
+    ss << res;
+    spdlog::info("I got this array {}", ss.str());
     
+    // test matplot
     std::vector<double> x = mp::linspace(0, 2 * mp::pi);
     std::vector<double> y = mp::transform(x, [](auto x) { return sin(x); });
 
